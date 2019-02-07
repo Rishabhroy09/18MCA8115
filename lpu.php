a7ee7d87c7af7e875e3be23e775aa0107930649a
Rishabhroy09 <47408837+Rishabhroy09@users.noreply.github.com>
2019-02-07 10:32:10 +0530

    Add files via upload

---
+++ lpu.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html> 
+ 
+<head> 
+<title>LOVELY PROFESSIONAL UNIVERSITY</title> 
+<link href="CU.css" rel="stylesheet"/> 
+</head> 
+ 
+<body bgcolor = "#fla5f5f5#000000" text = "black"> 
+ 
+<div class="first"> 
+<img src="img.jpg.jpg"  WIDTH= 270  HEIGHT = 510> 
+</div> 
+<div class="second" > 
+<table width="100% height="100%" cellpadding="10"> 
+<td width="200px"><center><a class="c" href="first.html"> <b>HOME</b></a></center></td> 
+<td width="200px"><center><a class="c" href = "gallery.html"> <b>Gallery</b> </a></center></td> 
+<td width="200px"><center><a class="c" href="contact.html"><b>Contact Us</b> </a></center></td> 
+</table> 
+</div> 
+<div class="third"> 
+<div class="sidebar1"> 
+<H1 ALIGN = "center" style="color: white"> NEWS</h1> 
+<marquee direction = "up" widht = "100px" height = "130px"> 
+<ul style="color:cornsilk"> 
+<li> Dear Students, 
+Humble Reminder! 
+In India, LPU is recognized by the University Grants Commission (UGC), the National Council for Teacher Education (NCTE)and the Council of Architecture (COA) and approved by the Pharmacy Council of India (PCI). It is also a member of the Association of Indian Universities (AIU). <br/> </li> 
+</marquee> 
+</div> 
+<div class="sidebar2"> 
+<h3 align = "center" style="color: White">Current Happening</h3> 
+<marquee direction = "up" widht = "100px" height = "130px" > 
+<ul style="color:cornsilk"> 
+<li>WORKSHOP ON “LINUX ESSENTIALS AND SERVICES” ORGANIZED BY CEACT</li> 
+<li>ADMISSION TO CERTIFICATE COURSES IN FOREIGN LANGUAGES</li> 
+</ul></marquee> 
+</div> 
+</div> 
+<div class="forth" color = "Red">
+  <h1 align="center"> Welcome To LOVELY PROFESSIONAL UNIVERSITY</h1>
+  <p align="center"><font size="6.5" color="white">Lovely Professional University is a private university situated in Jalandhar, Punjab, India. The university was established in 2005 by Lovely International Trust, under The Lovely Professional University Act, 2005 and started operation in 2006.</font></p> 
+</div> 
+</body> 
+</html> 
+
